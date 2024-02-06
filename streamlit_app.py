1b00694a0f8c24cd5f98083ecaa859847b08211e
sudheerbille <148851067+sudheerbille@users.noreply.github.com>
2024-02-06 18:42:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 import pandas
-st.markdown("<h1 style='text-align: left;'>Snowtasks Manager</h1>", unsafe_allow_html=True)
+st.title("<h1 style='text-align: left;'>Snowtasks Manager</h1>", unsafe_allow_html=True)
