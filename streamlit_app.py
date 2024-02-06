4b378a77d4c7a8bdd1c07fb347c64af9557022c0
sudheerbille <148851067+sudheerbille@users.noreply.github.com>
2024-02-06 18:41:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 import pandas
-st.markdown("<h1 style='text-align: left;'>Left-aligned Header</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: left;'>Snowtasks Manager</h1>", unsafe_allow_html=True)
